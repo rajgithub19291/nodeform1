5bde493402833701a10524d07c1a58ea4f23ff91
rajgithub19291 <80245357+rajgithub19291@users.noreply.github.com>
2022-04-08 14:44:10 +0530

    Add files via upload

---
+++ modal/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const user = new mongoose.Schema({
+    username:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    password:{
+        type:String
+    }
+});
+
+module.exports = mongoose.model('loginapp',user);
